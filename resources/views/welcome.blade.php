642002cd5aa88c6695a5f253d9258097be57f8ce
PramodiThilakarathne <pramodithilakarathne@gmail.com>
2024-07-10 11:34:57 +0530

    theme updated1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
+
 <body class="antialiased font-sans bg-gray-100">
 
 <!-- Header -->
